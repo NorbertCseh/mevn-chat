309dca352a7a555e22c318eac522148abd92a95a
ncseh <norbert.cseh@circeo.today>
2020-03-13 11:24:16 +0100

    dist, i cannot

---
+++ client/dist/precache-manifest.44f06670bfc5c9beb87149a22c49f40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510cc2553c021a01b529",
+    "url": "/css/app.bc0cfbef.css"
+  },
+  {
+    "revision": "28e7df7b73878905a5db0a224cdfbed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510cc2553c021a01b529",
+    "url": "/js/app.8c9fa955.js"
+  },
+  {
+    "revision": "f45a26c10589459837de",
+    "url": "/js/chunk-vendors.b4e3422c.js"
+  },
+  {
+    "revision": "e07dad9ca2f04ffa40a80acc420415d7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
