67bcd42254ea5c78ffc0d5b5f54ebcb63fa6d90a
ncseh <norbert.cseh@circeo.today>
2020-03-13 09:38:49 +0100

    deploy

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="container">
-    <div v-if="errors">{{errors}}</div>
-    <div v-if="user">User: {{user}}</div>
+    <div v-if="errors">{{ errors }}</div>
+    <div v-if="user">User: {{ user }}</div>
   </div>
 </template>
 
 <script>
-import Axios from "axios";
+import Axios from 'axios'
 export default {
   data() {
     return {
       user: null,
       errors: null
-    };
+    }
   },
   methods: {
     loadUserData() {
       Axios.get(`http://localhost:3000/api/room/${this.$route.params.user_id}`)
         .then((req, res) => {
-          this.user = res.data;
+          this.user = res.data
         })
         .catch(err => {
-          this.errors = err;
-          console.log(err);
-        });
+          this.errors = err
+        })
     }
   },
   mounted() {
-    this.loadUserData();
+    this.loadUserData()
   }
-};
+}
 </script>
 
-<style>
-</style>
+<style></style>
